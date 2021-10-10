eba84fe53c075d03dd7346ad2478aaa5d34f908d
Alexander Tamarez <alextq3036@gmail.com>
2021-10-10 01:28:11 -0400

    Setup the initial state of context to be the array of images objects pull from an API

---
+++ src/Context.js
@@ -0,0 +1,25 @@
+import React, {createContext, useState, useEffect} from 'react'
+
+const Context = createContext()
+
+function ContextProvider(props){
+    const [allPhotos, setAllPhotos] = useState([])
+
+    useEffect(()=>{
+        
+        fetch('https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json')  
+        .then(response => response.json())
+        .then(data => {
+            setAllPhotos(data)
+        })
+        
+    }, [])
+
+    return (
+        <Context.Provider value={{allPhotos}}>
+            {props.children}
+        </Context.Provider>
+        )
+}
+
+export {Context, ContextProvider}
