0f8cc74260b335fffd784f6a0773e73ab203ab66
Alexander Tamarez <alextq3036@gmail.com>
2021-10-10 14:46:18 -0400

    Mocked an order placement functionality that removed all items from the cart 3 seconds after pressing the place order button to simulate waiting for a response from an API

--- src/Context.js
+++ src/Context.js
@@ -1,21 +1,18 @@
-import React, {createContext, useState, useEffect} from 'react'
+import React, {useState, useEffect} from "react"
 
-const Context = createContext()
+const Context = React.createContext()
 
-function ContextProvider(props){
+function ContextProvider({children}) {
     const [allPhotos, setAllPhotos] = useState([])
     const [cartItems, setCartItems] = useState([])
-
-    useEffect(()=>{
-        
-        fetch('https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json')  
-        .then(response => response.json())
-        .then(data => {
-            setAllPhotos(data)
-        })
-        
+    
+    const url = "https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json"
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setAllPhotos(data))
     }, [])
-
+    
     function toggleFavorite(id) {
         const updatedArr = allPhotos.map(photo => {
             if(photo.id === id) {
@@ -26,20 +23,31 @@ function ContextProvider(props){
         
         setAllPhotos(updatedArr)
     }
-
-    function addToCart(newItem){
-        setCartItems(prevItem => [...prevItem, newItem])
+    
+    function addToCart(newItem) {
+        setCartItems(prevItems => [...prevItems, newItem])
     }
-
+    
     function removeFromCart(id) {
         setCartItems(prevItems => prevItems.filter(item => item.id !== id))
     }
     
+    function emptyCart() {
+        setCartItems([])
+    }
+    
     return (
-        <Context.Provider value={{allPhotos, toggleFavorite, addToCart, cartItems, removeFromCart}}>
-            {props.children}
+        <Context.Provider value={{
+            allPhotos, 
+            toggleFavorite, 
+            cartItems, 
+            addToCart, 
+            removeFromCart, 
+            emptyCart
+        }}>
+            {children}
         </Context.Provider>
-        )
+    )
 }
 
-export {Context, ContextProvider}
+export {ContextProvider, Context}
