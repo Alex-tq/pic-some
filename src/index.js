b5ba18aaa75a7d9d7cb47d4c5ef66d2df0d50eca
Alex-tq <alextq3036@gmail.com>
2021-10-18 00:02:45 -0400

    Changed the BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {BrowserRouter as Router} from 'react-router-dom'
+import {HashRouter as Router} from 'react-router-dom'
 import {ContextProvider} from './Context'
 
 ReactDOM.render(
   <ContextProvider>
-    <Router basename="/pic-some">
+    <Router>
       <App />
     </Router>
   </ContextProvider>,
